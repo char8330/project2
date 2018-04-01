12d53f4780aaa7263d47ee4f27c718396ef99bd5
char8330 <37884689+char8330@users.noreply.github.com>
2018-04-01 09:57:45 -0600

    Add files via upload

--- southdakota.php
+++ southdakota.php
@@ -11,6 +11,6 @@ class SouthDakota
         //CT310 unknown password, known hash
         "ct310" => "a6cebbf02cc311177c569525a0f119d7",
         "cjh" => "20ee80e63596799a1543bc9fd88d8878",
-        "jsearl" => "098f6bcd4621d373cade4e832627b4f6"
+        "jtperea" => "f869ce1c8414a264bb11e14a2c8850ed"
     );
 }
