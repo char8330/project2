322723a93a7f81c2b36b8ec1bbc4571b2c6b0c92
Jonathan Perea <jtperea@rams.colostate.edu>
2018-04-02 10:16:59 -0600

    Update southdakota.php

--- southdakota.php
+++ southdakota.php
@@ -1,119 +1,273 @@
 <?php
+
+use Model\Travel;
 /**
- * The South Dakota Controller.
- *
- * A basic controller example.  Has examples of how to set the
- * response body and status.
- *
- * @package  app
- * @extends  Controller
+ * Travel Controller
+ * use public function name() to add a page
+ * shares $layout = View::forge('southdakota/layoutfull'); with all pages
+ * checks if username and password is set on all pages
  */
 class Controller_SouthDakota extends Controller
 {
-
-
-//TEST comm w/ db
-
-
-   public function action_printCustomer()
-       {
-      
-       
-       $result = DB::select ('firstName')->from('students')->where('age',19)->execute();
-       $firstName = $result[0]['firstName'];
-       print($firstName);
-       
-       //return View::forge('southdakota/db', $views);
-       }
-
-//end TEST comm w/ db
-
-
 	/**
-	 * Index Page
-	 *
-	 * @access  public
-	 * @return  View
+	 * Shows a list of all travel items
 	 */
-	public function action_index() {
-        $views = array();
-        $views['header'] = View::forge('southdakota/header')->render();
-        $views['footer'] = View::forge('southdakota/footer')->render();
-        return View::forge('southdakota/index', $views);
-    }
+//		INDEX PAGE		//
+	public function action_index()
+	{
+		$layout = View::forge('southdakota/layoutfull');
 
-    /**
-	 * Login Page
-	 *
-	 * @access  public
-	 * @return  View
-	 */
-    public function action_login() {
-        $views = array();
-        $views['header'] = View::forge('southdakota/header')->render();
-        $views['footer'] = View::forge('southdakota/footer')->render();
-        return View::forge('southdakota/login', $views);
-    }
+		$content = View::forge('southdakota/index');
 
-    /**
-	 * Mt Rushmore Page
-	 *
-	 * @access  public
-	 * @return  View
-	 */
-    public function action_mt_rushmore() {
-        $views = array();
-        $views['header'] = View::forge('southdakota/header')->render();
-        $views['footer'] = View::forge('southdakota/footer')->render();
-        return View::forge('southdakota/mt_rushmore', $views);
-    }
+		$session = Session::instance();
 
-    /**
-    * Badlands National Park Page
-    *
-    * @access  public
-    * @return  View
-    */
-    public function action_badlands() {
-        $views = array();
-        $views['header'] = View::forge('southdakota/header')->render();
-        $views['footer'] = View::forge('southdakota/footer')->render();
-        return View::forge('southdakota/badlands', $views);
-    }
+                $username = $session->get('username');
 
-    /**
-     * Crazy Horse Page
-     *
-     * @access  public
-     * @return  View
-     */
-    public function action_crazy_horse() {
-        $views = array();
-        $views['header'] = View::forge('southdakota/header')->render();
-        $views['footer'] = View::forge('southdakota/footer')->render();
-        return View::forge('southdakota/crazy_horse', $views);
-    }
+                $id = $session->get('userid');
 
-    /**
-	 * About Page
-	 *
-	 * @access  public
-	 * @return  Response
-	 */
-    public function action_about() {
-        $views = array();
-        $views['header'] = View::forge('southdakota/header')->render();
-        $views['footer'] = View::forge('southdakota/footer')->render();
-        return View::forge('southdakota/about', $views);
-    }
+		$travels = travel::getAll();
 
-	/**
-	 * The 404 action for the application.
-	 *
-	 * @access  public
-	 * @return  Response
-	 */
-	public function action_404() {
+		$content->set_safe('travels', $travels);
+
+		//KEEPS USER LOGGED IN
+                if(isset($username) && isset($id))
+                {
+
+                        $content->set_safe('username',$username);
+
+                        $content->set_safe('id',$id);
+
+			$layout->content = Response::forge($content);
+
+                        return $layout;
+
+                }
+		//RETURN NORMAL LAYOUT
+		$layout->content = Response::forge($content);
+
+		return $layout;
+	}
+
+	
+//		ABOUT SECTION		//
+
+                public function action_about()
+        {
+                $layout = View::forge('southdakota/layoutfull');
+
+                $content = View::forge('southdakota/about');
+
+                $session = Session::instance();
+
+                $username = $session->get('username');
+
+                $id = $session->get('userid');
+
+                $travels = travel::getAll();
+
+                $content->set_safe('travels', $travels);
+
+		//KEEPS USERS LOGGED IN
+                if(isset($username) && isset($id))
+                {
+
+                        $content->set_safe('username',$username);
+
+                        $content->set_safe('id',$id);
+
+                        $layout->content = Response::forge($content);
+
+                        return $layout;
+
+                }
+		//RETURNS NORMAL LAYOUT
+                $layout->content = Response::forge($content);
+
+                return $layout;
+        }
+
+
+//		LOGIN SECTION		//
+	public function action_login()
+        {
+                $layout = View::forge('southdakota/layoutfull');
+
+		$status = 'success';
+
+                $content = View::forge('southdakota/login');;
+
+                $travels = travel::getAll();
+
+                $content->set_safe('travels', $travels);
+
+		$content->set_safe('status', $status);
+
+                $layout->content = Response::forge($content);
+         
+                return $layout;
+        }
+
+//		CHECKS SUCCESSFUL LOGIN			//
+        public function action_checkLogin()
+        {
+		$layout = View::forge('southdakota/layoutfull');
+
+                $username = Input::post('username');
+                $password = Input::post('password');
+
+		//hardcoded users & passwords
+                if(($username === 'ct310' && md5($password) === 'a6cebbf02cc311177c569525a0f119d7') || ($username === 'jtperea' && md5($password) === 'f869ce1c8414a264bb11e14a2c8850ed') )
+                {
+                        Session::create();
+
+                        Session::set('username', $username);
+
+                        Session::set('userid', 12345);
+
+                        $content = View::forge('southdakota/index');
+
+			$travels = travel::getAll();
+
+	                $content->set_safe('travels', $travels);
+
+			$layout->content = Response::forge($content);
+
+                        return $layout;
+                }
+                else
+                {
+
+			$content = View::forge('southdakota/login');
+
+			$travels = travel::getAll();
+
+                        $content->set_safe('travels', $travels);
+
+                        $content->set_safe('status','error');
+
+			$layout->content = Response::forge($content);
+
+                        return $layout;
+                }
+
+        }
+
+//			LOGOUT PAGE		//
+        public function action_logout()
+        {
+                $session = Session::instance();
+
+                $session->destroy();
+
+                $content = View::forge('travel/logout');
+
+                return $content;
+        }
+
+//			FOR ADMIN CONTROL		//
+        public function action_view($id)
+        {
+                $session = Session::instance();
+                $username = $session->get('username');
+
+		//only viewable by admin accounts
+                if($username === 'ct310' || $username === 'jtperea')
+                {		
+
+                	$layout = View::forge('southdakota/layoutfull');
+
+	                $content = View::forge('southdakota/view');
+
+        	        $tr = new travel($id);
+
+                	$content->set_safe('tr', $tr);
+
+	                $layout->content = Response::forge($content);
+
+        	        return $layout;
+		}
+		//else show nothing
+        }
+
+	        public function action_adminIndex()
+        {
+		$session = Session::instance();
+                $username = $session->get('username');
+
+		if($username === 'ct310' || $username === 'jtperea')
+		{
+                	$layout = View::forge('southdakota/layoutfull');
+
+        	        $content = View::forge('southdakota/adminIndex');
+	
+                	$travels = travel::getAll();
+
+        	        $content->set_safe('travels', $travels);
+
+	                $layout->content = Response::forge($content);
+
+                	return $layout;
+		}
+		//else shows nothing
+        }
+
+	       public function get_addEdit($id = null)
+        {
+                $session = Session::instance();
+                $username = $session->get('username');
+
+                if($username === 'ct310' || $username === 'jtperea')
+                {
+                        $layout = View::forge('southdakota/layoutfull');
+
+                        $content = View::forge('southdakota/addEdit');
+
+                        $tr = new travel($id);
+
+                        $content->set_safe('tr', $tr);
+
+                        $layout->content = Response::forge($content);
+
+                        return $layout;
+                }
+                //else shows nothing
+        }
+
+        public function post_addEdit($id = null)
+        {
+                $session = Session::instance();
+                $username = $session->get('username');
+	
+                if($username === 'ct310' || $username === 'jtperea')
+                {
+                	$tr = new travel($id);
+	                $tr->id = $_POST['id'];
+          	        $tr->name = $_POST['name'];
+                	$tr->save();
+	                Response::redirect('index.php/southdakota/adminIndex');
+		}
+		//else shows nothing
+        }
+
+        public function action_delete($id)
+        {
+                $session = Session::instance();
+                $username = $session->get('username');
+
+                if($username === 'ct310' || $username === 'jtperea')
+		{
+                	$tr = new travel($id);
+	                $tr->delete();
+        	        Response::redirect('index.php/southdakota/adminIndex');
+		}
+		//else shows nothing
+        }
+
+//			END OF ADMIN CONTROL		//
+
+//			ERROR PAGE			//
+	public function action_404()
+	{
 		return Response::forge(Presenter::forge('welcome/404'), 404);
 	}
 }
