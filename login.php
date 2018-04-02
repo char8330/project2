d88a5eb5d2d144b68f43cab51496f869b11332cc
Jonathan Perea <jtperea@rams.colostate.edu>
2018-04-02 10:30:09 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,64 +1,18 @@
-<?php echo $header ?>
+<html>
+<head>
+	<title>Login Page</title>
+</head>
+<body>
+	<h1>Login</h1>
+	<?php if($status === 'error') {?>
+		<p>Incorrect details entered. Please try again.</p>
+	<?php } ?>
+	<form action="checkLogin" method="POST">
+		<input type="text" name="username" placeholder="username"/> 
+		<input type="password" name="password" placeholder="password"/> 
+		<input type="submit" value="submit">
+	</form>
 
-<!-- HEADERS -->
-<header>
-    <h1 style="background-color: #53C7E9; color: #28829C">YOUR ACCOUNT</h1>
-</header>
-</br>
-<header>
-    <h3 style="color: #9C6128;">SIGN IN</h3>
-</header>
-
-</br>
-
-<!-- sign in -->
-<?php $f = "
-<FORM NAME ='form1' METHOD ='POST'>
-    <INPUT TYPE = 'text' name = 'username' placeholder ='username'>
-    <INPUT TYPE = 'password'  name = 'password' placeholder ='password'>
-    <INPUT TYPE = 'Submit' Name = 'Submit1' value = 'Enter'>
-</FORM>";
-
-$submitted_username = $submitted_pwd = '';
-$first_login = true;
-$authorized = false;
-
-if (isset($_POST ['Submit1'])){
-    $submitted_username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
-    $submitted_pwd = md5(filter_var($_POST['password'], FILTER_SANITIZE_STRING));
-    $first_login = false;
-}
-
-$users = array(
-    //CT310 unknown password, known hash
-    //TODO: ADD Customer: Bob Ross
-    "ct310" => "a6cebbf02cc311177c569525a0f119d7",
-    "cjh" => "20ee80e63596799a1543bc9fd88d8878",
-    "jtperea" => "f869ce1c8414a264bb11e14a2c8850ed");
-
-date_default_timezone_set("America/Denver");
-
-foreach ($users as $usr => $passwd_hash) {
-    if($submitted_username === $usr && $submitted_pwd === $passwd_hash) {
-        $authorized = true;
-        break;
-    }
-}
-
-if (!$authorized && !$first_login) {
-    echo $f;
-    echo "</br>Permission Denied</br>";
-    $t = date('l jS \of F Y h:i:s A');
-    echo($t . "</br>");
-} elseif ($authorized){
-    echo "Welcome " . $usr . "!   <br>";$t=date('l jS \of F Y h:i:s A');
-    echo($t . "</br>");
-//attempt to stay logged in - set authorized in the session TODO: FIND OUT WHEN TO END SESSION
-//TODO: Add Logout/update to jonathan's login/logout code?
-Session::create(); 
-Session::set('authorized', $authorized);
-} else {
-    echo $f;
-}
-
-echo $footer ?>
+	
+</body>
+</html>
