1bfc30503efbf9f8c4f6df0a2a4f509da9fef3fb
Jonathan Perea <jtperea@rams.colostate.edu>
2018-04-02 10:23:06 -0600

    Update about.php

--- about.php
+++ about.php
@@ -1,4 +1,3 @@
-<?php echo $header ?>
 <header>
     <h1 style="background-color: #53C7E9; color: #28829C">About Us</h1>
 </header>
@@ -11,6 +10,3 @@
 For more information about us and why you should go travel the US now contact us at 719-309-7222 or harless_serea@travel@travel.net
     <br>
     <br>
-
-<?php echo $footer ?>
-
