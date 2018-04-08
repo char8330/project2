69810e1c7ed646056970d85aaafb9f6f4cf39f76
char8330 <37884689+char8330@users.noreply.github.com>
2018-04-08 17:50:37 -0600

    Add files via upload

--- southdakota.php
+++ southdakota.php
@@ -2,6 +2,7 @@
 use Model\Travel;
 use Model\ormtravel; //new
 use Model\ormbrochure; 
+use Model\ormattraction;
 /**
  * Travel Controller
  * use public function name() to add a page
@@ -15,12 +16,74 @@ class Controller_SouthDakota extends Controller
 	 */
 	 
 
-	 //new
 	 
 	 
 	 
-	 public function action_indexc()
+	 //all attractions
+	  public function action_allattractions()
 	{
+		//load the layout
+		$layout = View::forge('southdakota/layoutfull');
+		
+		//load the view
+		$content = View::forge('southdakota/allattractions'); //TODO: change this later to commentes/badlands
+                
+                
+		//get all courses using the ORM object
+		$courses = Ormattraction::find('all');
+
+		$AttractionString;
+		$JustTitle;
+		
+
+		//this loop converts all courses to a single string and stores them in $CoursesString
+		foreach($courses as $key=>$course)
+		{
+			$AttractionString[$key] = $course['attID']." ".$course['firstName'].": ".$course['descriptionName'].": ".$course['img']; //add attraction col
+			$JustTitle[$key] = " ".$course['firstName'];
+		}
+
+		//set the courses to the view for printing
+		$content->set_safe('demos', $AttractionString); //demos? 
+		$content->set_safe('demos_title', $JustTitle);
+		
+		$content->set_safe('att', $courses);
+		
+		//$content->set_safe('demos', $courses);
+		
+
+		//forge inner view
+		$layout->content = Response::forge($content);
+		
+		
+		
+		 //loop through attractions and add
+                 //$exists = File::exists('/s/bach/k/under/char8330/fuel/app/views/southdakota/', 'testthisone123.php');
+	         //if($exists === false){
+                //foreach($JustTitle as $demo){
+                     //echo $demo;}
+			//File::create(DOCROOT, $demo+'.php', 'Contents for file.');}
+                //endforeach; }
+		
+		
+
+		return $layout;
+		
+	}
+	
+	//end all attractions 
+	 
+	 
+	 public function action_indexc($id)
+	{//new add4/8 - $id^^^ and below...
+	
+	
+	
+	//end new add 4/8
+	
+	
+	
+	
 		//load the layout
 		$layout = View::forge('southdakota/layoutfull');
 		
@@ -28,8 +91,19 @@ class Controller_SouthDakota extends Controller
 		$content = View::forge('southdakota/indexc'); //TODO: change this later to commentes/badlands
                 
                 
+                
+                
+                $demo = Ormattraction::find($id); //extract attraction from id 
+                
+
+		$content->set_safe('attractioninfo', $demo);
+
+		
+                
+                
+                
 		//get all courses using the ORM object
-		$courses = Ormtravel::find('all');
+		$courses = Ormtravel::find('all'); //this is the array
 
 		$CoursesString;
 		$JustComments;
@@ -37,13 +111,18 @@ class Controller_SouthDakota extends Controller
 		//this loop converts all courses to a single string and stores them in $CoursesString
 		foreach($courses as $key=>$course)
 		{
-			$CoursesString[$key] = $course['id']." ".$course['user'].": ".$course['comm']; //add attraction col
+			$CoursesString[$key] = $course['user'].": ".$course['comm']; //add attraction col
+			//$CoursesString[$key] = $course['id']." ".$course['user'].": ".$course['comm']; //add attraction col
 			$JustComments[$key] = " ".$course['comm'];
+			$JustIds[$key] = $course['id'];
 		}
 
 		//set the courses to the view for printing
 		$content->set_safe('demos', $CoursesString); //demos? 
 		$content->set_safe('demos_comms', $JustComments);
+		$content->set_safe('demos_ids', $JustIds);
+		
+		$content->set_safe('att', $courses);
 
 		//forge inner view
 		$layout->content = Response::forge($content);
@@ -51,7 +130,9 @@ class Controller_SouthDakota extends Controller
 		return $layout;
 		
 	}
-
+	
+	
+// 	
 	//public function get_add() //not in use atm 
 	//{
 		//load the layout
@@ -65,11 +146,66 @@ class Controller_SouthDakota extends Controller
 
 	//	return $layout;
 	//}
+	
+	
+                public function action_edit($id)
+	{ 
+                        $entry = ormtravel::find($id);
+                        //$entry->id = 'My first edit';
+                        //$entry->user = $username; //TODO: edits keep name of org poster or admin who edited? 0- attach to each $demo 
+                        $entry->comm = 'fish edited';
+                        $entry->save();
+                // attach urls to correct attraction index ....indexc/attractionttitle ideally 
+                Response::redirect('index.php/southdakota/allattractions');
+	}
+	
+	
+		public function action_delete($id)
+	{      // if (isset($_POST['delete'])) {
+		$a = ormtravel::find($id);
+		//$gone = $demo->__toString();
+		$a->delete();
+		//Session::set_flash('flash', array('msg' => "Demo $gone Deleted!", 'type' => 'success'));
+		//}
+		Response::redirect('index.php/southdakota/allattractions');
+		
+		
+	}
+	
+	//brochure delete/edit
+	
+	
+                public function action_editb($id)
+	{ 
+                        $b = ormbrochure::find($id);
+                        //$entry->id = 'My first edit';
+                        //$entry->user = $username; //TODO: edits keep name of org poster or admin who edited? 0- attach to each $demo 
+                        $b->quantity = 0;
+                        $b->save();
+                // attach urls to correct attraction index ....indexc/attractionttitle ideally 
+                Response::redirect('index.php/southdakota/indexbrochure');
+	}
+	
+	
+		public function action_deleteb($id)
+	{      // if (isset($_POST['delete'])) {
+		$b = ormbrochure::find($id);
+		//$gone = $demo->__toString();
+		$b->delete();
+		//Session::set_flash('flash', array('msg' => "Demo $gone Deleted!", 'type' => 'success'));
+		//}
+		Response::redirect('index.php/southdakota/indexbrochure');
+		
+		
+	}
 
-	public function post_indexc() //prev post add
+	public function post_indexc($id) //prev post add
 	{
+	
+	
 	 $session = Session::instance();
 	$username = $session->get('username');
+	//if logged in 
 	if($username === 'ct310' || $username === 'jtperea' || $username === 'cjh' || $username === 'aaronadmin'){
 	
                 $result = DB::select ('comm')->from('comments')->execute(); // TODO: check if empty/no previous comments
@@ -80,6 +216,7 @@ class Controller_SouthDakota extends Controller
 		//extract course name, number and assignments from the input parameters
 		//$name = $_POST['id'];
 		//$number = $username; // $_POST['name']; // make user 
+		if (isset($_POST['comment2'])) {
 		$assignments = $_POST['comment2'];
 
 		//create a new ORM object and populate it
@@ -90,11 +227,15 @@ class Controller_SouthDakota extends Controller
 
 		//save the ORM object
 		$new->save();
-
-		//reload the index page with the newly saved view
-		Response::redirect('index.php/southdakota/indexc.php');
+		}
 		
-		}else{  Response::redirect('index.php/southdakota/login.php');} // make another view to add line: "Login to make comments"
+		
+		
+   
+		//reload the index page with the newly saved view
+		Response::redirect('index.php/southdakota/indexc/'.$id);
+		
+		}else{  Response::redirect('index.php/southdakota/login');} // make another view to add line: "Login to make comments"
 	}
 	 
 
@@ -120,19 +261,14 @@ class Controller_SouthDakota extends Controller
 		$content = View::forge('southdakota/indexbrochure'); //TODO: change this later to commentes/badlands
                 
                 
+                $session = Session::instance();
+                $username = $session->get('username');
+	
 		//get all courses using the ORM object
 		$courses = Ormbrochure::find('all');
+                $content->set_safe('brochures', $courses); //demos? 
 
-		$CoursesString;
-
-		//this loop converts all courses to a single string and stores them in $CoursesString
-		foreach($courses as $key=>$course)
-		{
-			$CoursesString[$key] = $course['id']." ".$course['user'].": ".$course['quantity']; //add attraction col
-		}
-
-		//set the courses to the view for printing
-		$content->set_safe('demos', $CoursesString); //demos? 
+                
 
 		//forge inner view
 		$layout->content = Response::forge($content);
@@ -141,7 +277,7 @@ class Controller_SouthDakota extends Controller
 		
 	}
 
-	public function get_add() //not in use atm 
+	public function get_add($attid) //not in use atm 
 	{
 		//load the layout
 		$layout = View::forge('southdakota/layoutfull');
@@ -155,7 +291,7 @@ class Controller_SouthDakota extends Controller
 		return $layout;
 	}
 
-	public function post_add() //prev post add
+	public function post_add($attid) //prev post add
 	{
 	 $session = Session::instance();
 	$username = $session->get('username');
@@ -172,10 +308,14 @@ class Controller_SouthDakota extends Controller
 
 
 		//create a new ORM object and populate it
-		$new = new ormtravel();
+		$new = new ormbrochure();
 		$new->id = $name;
 		$new->user = $username;
 		$new->quantity = $assignments;
+		
+		//find attraction
+		$att_name = ormattraction::find($attid);
+		$new->attraction = $att_name['firstName'];
 
 		//save the ORM object
 		$new->save();
@@ -201,63 +341,6 @@ class Controller_SouthDakota extends Controller
 	 
 	 
 	 
-	 
-	 
-	 
-	  public function get_commented() //TODO: MOVE CODE FROM commented.php to these 2 methods?? 
-	{
-                $session = Session::instance();
-                $username = $session->get('username');
-                if($username === 'ct310' || $username === 'jtperea' || $username === 'cjh' || $username === 'aaronadmin')
-                { echo "Make comments here";
-                        $layout = View::forge('southdakota/layoutfull');
-                        $content = View::forge('southdakota/attraction1');
-                        $tr = new travel($id);
-                        $content->set_safe('tr', $tr);
-                        $layout->content = Response::forge($content);
-                        return $layout;
-                }//else{echo "Login to make comments";}
-                //else shows statement "login to make comments" 
-                 
-        }
-        public function post_commented($id = null)
-        {
-                $session = Session::instance();
-                $username = $session->get('username');
-	
-                if($username === 'ct310' || $username === 'jtperea' || $username === 'cjh' || $username === 'aaronadmin')
-                {
-                	$tr = new travel($id);
-	                $tr->id = $_POST['id'];
-          	        $tr->name = $_POST['name'];
-                	$tr->save();
-	                Response::redirect('index.php/southdakota/attraction1');
-		}
-		//else shows nothing
-        }
-	 public function action_attraction1()
-	{
-		$layout = View::forge('southdakota/layoutfull');
-		$content = View::forge('southdakota/attraction1');
-		//$content = View::forge('southdakota/commented');
-		$session = Session::instance();
-                $username = $session->get('username');
-                $id = $session->get('userid');
-		$travels = travel::getAll();
-		$content->set_safe('travels', $travels);
-		//KEEPS USER LOGGED IN
-                if(isset($username) && isset($id))
-                {       //allow comments
-                        //echo "YOU ARE LOGGED IN AND CAN MAKE COMMENTS";
-                        $content->set_safe('username',$username);
-                        $content->set_safe('id',$id);
-			$layout->content = Response::forge($content);
-                        return $layout;
-                }//else{echo "LOGIN TO MAKE COMMENTS";} //block comments
-		//RETURN NORMAL LAYOUT
-		$layout->content = Response::forge($content);
-		return $layout;
-	}
 	 
 	 
 //		INDEX PAGE		//
@@ -355,7 +438,7 @@ class Controller_SouthDakota extends Controller
                 return $content;
         }
 //			FOR ADMIN CONTROL		//
-        public function action_view($id)
+        public function action_view2($id)
         {
                 $session = Session::instance();
                 $username = $session->get('username');
@@ -418,7 +501,7 @@ class Controller_SouthDakota extends Controller
 		}
 		//else shows nothing
         }
-        public function action_delete($id)
+        public function action_delete2($id)
         {
                 $session = Session::instance();
                 $username = $session->get('username');
