d71aaa3a5ebb9ff28562e0ef811bfbb1aa39b66a
Jonathan Perea <jtperea@rams.colostate.edu>
2018-04-08 21:23:26 -0600

    Update layoutfull.php

--- layoutfull.php
+++ layoutfull.php
@@ -6,39 +6,38 @@
                 <?php echo Asset::css('southdakota.css'); ?>
                  
         </head>
-        <div class="sidebar">
+        <div class="top-navbar">
                 <h1>
-                        <ul><br><br><br><br>
+                        <ul>
                                 <li><a href="<?=Uri::create('index.php/southdakota/index'); ?>">Home</a></li>
-                                <br>
+                                <!--<br>-->
 
                                 <li><a href="<?=Uri::create('index.php/southdakota/login'); ?>">Login</a></li>
-                                <br>
-
+                                <!--<br>-->
                                 <li><a href="<?=Uri::create('index.php/southdakota/about'); ?>">About Us</a></li>
-                                <br>
+                                <!--<br>-->
 
                                 <li><a href="<?=Uri::create('index.php/southdakota/adminIndex'); ?>">Admin</a></li>
-                                <br>
+                                <!--<br>-->
                                 
                                 
                                  <li><a href="<?=Uri::create('index.php/southdakota/allattractions'); ?>"> All Attractions</a></li>
-                                <br>
+                                <!--<br>-->
                                 
                                  <li><a href="<?=Uri::create('index.php/southdakota/indexbrochure'); ?>"> Cart</a></li>
-                                <br>
+                                <!--<br>-->
                               
                                 
                                 
-                        </ul>   
+                        </ul> 
                         </h1>
                 </div>
         <body>
                 <div id="header">
-					<h1>Travel Site
+                    <h1>Travel Site
                         <div id="logo">
-							<?php echo Asset::img("SC_Logo.png",array("width" => "90"));?>
-				</div></h1>
+                            <?php echo Asset::img("SC_Logo.png",array("width" => "90"));?>
+                </div></h1>
 
                 </div> 
                 <!--$content is called by the controller located in fuel/app/classes/controller-->
@@ -49,7 +48,7 @@
                <br>
                 <div id="footer">
                         This site is part of a <a href="https://www.cs.colostate.edu/~ct310/">CT310</a> Course Project <a href="<?=Uri::create('index.php/southdakota/logout'); ?>">Logout</a></li>
-			
+            
                 </div>
         </body>
 </html>
