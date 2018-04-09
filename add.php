fdbb00966f8b5652dd68d1d18131b7dc7d6bd0cc
char8330 <37884689+char8330@users.noreply.github.com>
2018-04-08 19:08:55 -0600

    Add files via upload

--- add.php
+++ add.php
@@ -1,16 +1,10 @@
 <h2>
-	<a href="<?=Uri::create('index.php/southdakota/indexbrochure'); ?>">Brochures</a>
-	» Add
+	<a href="<?=Uri::create('index.php/southdakota/indexbrochure'); ?>">View Cart</a>
+	» Add Brochures
 </h2>
 <div class="h2Content">
-	Add Comment
+	
 	<form method="post">
-		<label for="id">User Id</label>
-		<input type="text" name="id" />
-		<br />
-		<label for="id">Username</label>
-		<input type="text" name="name" />
-		<br />
 		<label for="id">Quantity</label>
 		<input type="text" name="quantity" />
 		<br />
--- indexbrochure.php
+++ indexbrochure.php
@@ -1,26 +1,28 @@
 <h2>
-	Shop:
+	Shopping Cart
 	<span class="floatClear"></span>
 </h2>
-
-<h2>Cart</h2>
 
 <div class="h2Content">
 	<?php foreach($brochures as $b):  //TODO: ONLY GET ELEMENTS FROM 1 USER who is LOGGED in - check logged in too -- CURRENTLY BEING BAD?>
 	
-	                <?php $session = Session::instance();
-                        $username = $session->get('username'); 
-                        $usercheck = $b['user']; ?>
-                        <?php if ($username = $usercheck): ?>
 	
 	                <form method="post">
 	                <?php echo $b['attraction'].": Quantity" ;?>
-			<input type="text" value="<?php echo $b['quantity']." ".$usercheck." ".$username;?>">
+			<input type="text" value="<?php echo $b['quantity'];?>">
 			<a href="<?=Uri::create('index.php/southdakota/deleteb/'.$b['id']); ?>"
 		   onclick="return confirm('Are you sure you want to delete this?');">&#x1f5d1; Delete</a>
 		   <a href="<?=Uri::create('index.php/southdakota/editb/'.$b['id']); ?>">&#x270E; Save Edits</a>
 		   <br>
-		   <?php else: echo 'cart empty'?>
-		   <?php endif ?>
+		   
+		   
 	<?php endforeach; ?>
+	<br>
+	<br>
+	
+	<div class="h2Content">
+		<a href="<?=Uri::create('index.php/southdakota/order/'.$b['id']); ?>"
+		   onclick="return confirm('Are you sure you want to place this order?');">ORDER NOW</a>
+	</form> 
+</div>
 </div>
