7a4c90e11c221bd62d5a00a28c591d25ec046699
Jonathan Perea <jtperea@rams.colostate.edu>
2018-04-09 10:39:26 -0600

    Create reset.php

---
+++ reset.php
@@ -0,0 +1,6 @@
+<p>	Welcome to your password reset</p>
+	<form method="post" action="login">
+	Enter new password here: <input type="text" name="password" placeholder="New Password"> 
+	<input type="submit" value="Submit">
+	</form>		
+  	<!-- Need to grab either email or userID from database and associate it to the session, then make a variable to store the password and update the database with the new password -->
