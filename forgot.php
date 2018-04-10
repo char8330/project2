f4fd3d7e17c18040b07a5249b55046c13f085fa4
Jonathan Perea <jtperea@rams.colostate.edu>
2018-04-10 17:10:20 -0600

    Update forgot.php

--- forgot.php
+++ forgot.php
@@ -3,8 +3,8 @@
 </head>
 <body>
 	<h1>Password Reset</h1>
-		<form method="post" action="forgot">
-		<!--<form method ="post" action="forget2">-->
+		<!--<form method="post" action="forgot">-->
+		<form method ="post" action="forget2">
 		Enter your email here to request password reset: <input type="text" name="email" placeholder="Enter Email"> 
 		<input type="hidden" name="ah" value=<?php// echo $key; ?> >
 		<input type="submit" value="Submit">
