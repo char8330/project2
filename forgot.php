008edc1163e20a4a9154c1f68e32c11731d59697
Jonathan Perea <jtperea@rams.colostate.edu>
2018-04-09 18:09:00 -0600

    Update forgot.php

--- forgot.php
+++ forgot.php
@@ -4,11 +4,12 @@
 <body>
 	<h1>Password Reset</h1>
 	<?php
+		
 		$mailTo = Input::param("email");
 		if(isset($mailTo)){
 
 		$subject = "Password Reset";
-		$content = 'Please follow this link to reset password https://www.cs.colostate.edu/~jtperea/ct310/index.php/southdakota/reset';
+		$content = 'Please follow this link to reset password https://www.cs.colostate.edu/~jtperea/ct310/index.php/southdakota/reset/'. $secretKey;
 		error_reporting(0);
 		if(mail($mailTo, $subject, $content)){
 				echo "<p>Reset link sent to $mailTo</p>";
