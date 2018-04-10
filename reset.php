93dda5e516564e7e421df0170936ff49c622a310
Jonathan Perea <jtperea@rams.colostate.edu>
2018-04-09 18:09:29 -0600

    Update reset.php

--- reset.php
+++ reset.php
@@ -1,6 +1,16 @@
 <p>	Welcome to your password reset</p>
+<?php;
+	$em = Session::get("mailTo");
+	//$em = Input::param("email");
+	if(isset($em)){
+		echo "$em";
+	} else {
+
+		echo "Nope";
+	}
+?>
 	<form method="post" action="login">
 	Enter new password here: <input type="text" name="password" placeholder="New Password"> 
 	<input type="submit" value="Submit">
 	</form>		
-  	<!-- Need to grab either email or userID from database and associate it to the session, then make a variable to store the password and update the database with the new password -->
+	<!-- Need to grab either email or userID from database and associate it to the session, then make a variable to store the password and update the database with the new password -->
