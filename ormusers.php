dbf4662bcb585395bdce49d51da54ee998dda4df
Jonathan Perea <jtperea@rams.colostate.edu>
2018-04-09 22:57:06 -0600

    Update ormusers.php

--- ormusers.php
+++ ormusers.php
@@ -2,7 +2,7 @@
 namespace Model;
 class ormusers extends \Orm\Model
 {
-	protected static $_properties = 'username', 'accessLevel', 'email', 'PassHash'); //TODO: ADD attraction
+	protected static $_properties = array('username', 'email', 'PassHash', 'secretKey'); //TODO: ADD attraction
 	protected static $_table_name = 'users';
 	protected static $_primary_key = array('username');
 }
