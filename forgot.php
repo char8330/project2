13f93d0c7c427036a9dbe08a2cbb2e87161da3c3
Jonathan Perea <jtperea@rams.colostate.edu>
2018-04-09 23:03:57 -0600

    Update forgot.php

--- forgot.php
+++ forgot.php
@@ -3,30 +3,11 @@
 </head>
 <body>
 	<h1>Password Reset</h1>
-	<?php
-		
-		$mailTo = Input::param("email");
-		if(isset($mailTo)){
-
-		$subject = "Password Reset";
-		$content = 'Please follow this link to reset password https://www.cs.colostate.edu/~jtperea/ct310/index.php/southdakota/reset/'. $secretKey;
-		error_reporting(0);
-		if(mail($mailTo, $subject, $content)){
-				echo "<p>Reset link sent to $mailTo</p>";
-			}
-			else {
-				echo "<p>There was an error trying to reset your password.</p>\n";
-			}
-	}
-		else{
-			echo "<form method=\"post\" action=\"forgot\">\n";
-			?>
+		<form method="post" action="forgot">
+		<!--<form method ="post" action="forget2">-->
 		Enter your email here to request password reset: <input type="text" name="email" placeholder="Enter Email"> 
+		<input type="hidden" name="ah" value=<?php// echo $key; ?> >
 		<input type="submit" value="Submit">
+		<a href = "<?=Uri::create('index.php/southdakota/forget2')?>">Reset</a>
 		</form>		
-	<?php
-	}
-
-	echo "<br><br>";
-	?>
 </body>
