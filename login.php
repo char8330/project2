652fbf8fa51f2ffc2de1e1dcd4e70dc63985ec2b
Jonathan Perea <jtperea@rams.colostate.edu>
2018-04-09 23:01:54 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+<html>
 <head>
 	<title>Login Page</title>
 </head>
@@ -5,12 +6,15 @@
 	<h1>Login</h1>
 	<?php if($status === 'error') {?>
 		<p>Incorrect details entered. Please try again.</p>
-	<?php }?>
+	<?php } ?>
 	<form action="checkLogin" method="POST">
-		<input type="text" name="username" placeholder="username"/> 
-		<input type="password" name="password" placeholder="password"/> 
+		Username: <br>
+		<input type="text" name="username" placeholder="Please enter username"/> <br> </br>
+		Password: <br>
+		<input type="password" name="password" placeholder="Please enter password"/> <br> </br>
 		<input type="submit" value="submit">
 	</form>
-	<h3><a href="<?=Uri::create('index.php/southdakota/forgot'); ?>">Forgot Password?</a></h3>
+
 	
 </body>
+</html>
