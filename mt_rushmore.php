0d4a220371d0c6ae0e5bf950754bfdea12911190
char8330 <37884689+char8330@users.noreply.github.com>
2018-04-09 23:16:52 -0600

    Update mt_rushmore.php

--- mt_rushmore.php
+++ mt_rushmore.php
@@ -4,7 +4,7 @@
 </br>
 <div>
     <!-- BODY TEXT -->
- Mount Rushmore is located in the Black Hills of South Dakota. It is best known for Mount Rushmore National Memorial, the large carvings of the president's faces (George Washington, Thomas Jefferson, Theodore Roosevelt, and Abraham Lincoln) that adorn the rock face. The faces span 60 feet tall and the width of a single eye is about 11 feet. The work of carving the faces lasted from 1927 to 1941, taking a total of 14 years to complete. A skilled sculpture named Gutzon Borglum is the creator of the monolithic piece, and he chose the faces to carve there as a celebration of America's 150 years as a free country. The faces are made of granite and well worth visiting, attracting around 3 million visitors annually.
+ Mount Rushmore is located in the Black Hills of South Dakota. It is best known for Mount Rushmore National Memorial, the large carvings of presidents faces (George Washington, Thomas Jefferson, Theodore Roosevelt, and Abraham Lincoln) that adorn the rock face. The faces span 60 feet tall and the width of a single eye is about 11 feet. The work of carving the faces lasted from 1927 to 1941, taking a total of 14 years to complete. A skilled sculpture named Gutzon Borglum is the creator of the monolithic piece, and he chose the faces to carve there as a celebration of 150 years as a free country. The faces are made of granite and well worth visiting, attracting around 3 million visitors annually.
 <br>
 <br>
 The memorial is open from 5 A.M. - 11:30 P.M. most days of the year. Entry is free however the parking fee is $10 for private vehicles. 
