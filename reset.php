2f77b78005993fd885102af3f89fadb5a0a06309
Jonathan Perea <jtperea@rams.colostate.edu>
2018-04-10 17:10:48 -0600

    Update reset.php

--- reset.php
+++ reset.php
@@ -1,5 +1,7 @@
 <p>	Welcome to your password reset</p>
-	<form method="post" action="login">
-	Enter new password here: <input type="password" name="pass" placeholder="New Password"> 
+
+	<form method="post" action="../newpass">
+	Enter new password here: <input type="password" name="pass3" placeholder="New Password"> 
+	<input type="hidden" name="name" value="<?php echo $name ?>">
 	<input type="submit" value="Submit">
 	</form>		
